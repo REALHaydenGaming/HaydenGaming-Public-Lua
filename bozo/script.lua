98a67c1e52ec0a9b4db001a8d0971a4d36e322b8
Not_HaydenGaming <HaydenGamingFNF@protonmail.com>
2022-11-22 17:55:28 -0500

    ch r omat ic ab erra ti on r e al
    - mod link including the goofy fun fact
    - chromatic aberration whoah!

--- bozo/script.lua
+++ bozo/script.lua
@@ -1,16 +1,24 @@
 -- THIS ONLY WORKS ON OS ENGINE 1.2.0!!!
+-- goofy ass mod link so you can port it over: https://gamebanana.com/dl/831861 (owner is a zoophile and you shouldnt support them!)
+-- fun fact: worked on it until my gj account got banned for no reason (Thanks Feroz!)
+
+local angleTween = 360
+local what = 0.0
 
 function onCreate()
-    -- idk whata add.
+    addChromaticAbberationEffect("camHUD", 0.007)
 end
 
+function onUpdatePost()
+	what = what * 0.002
+end
 function onBeatHit()
     if curBeat == 0 then
          doTweenZoom('dad?', 'camGame', 1.4, 8, 'sineIn')
          doTweenAlpha('fuckyouhud', 'camHUD', 0, 0.5, 'quadOut')
     end
     if curBeat == 64 then
-	ballSackReal = true
+        ballSackReal = true
         cancelTween('dad?')
         doTweenAlpha('fuckyouhud', 'camHUD', 1, 0.5, 'quadOut')
         doTweenZoom('fatherMoment', 'camGame', 0.8, 1, 'cubeOut')
@@ -22,21 +30,20 @@ function onBeatHit()
 								
 								-- opponent strums go here
 								noteTweenX('wtfMain5', 0, 415, 2.3, 'expoOut')
-								noteTweenX('wtfMain6', 1, 525, 2.3, 'expoOut')
-								noteTweenX('wtfMain7', 2, 625, 2.3, 'expoOut')
-								noteTweenX('wtfMain8', 3, 725, 2.3, 'expoOut')
+								noteTweenX('wtfMain6', 1, 515, 2.3, 'expoOut')
+								noteTweenX('wtfMain7', 2, 615, 2.3, 'expoOut')
+								noteTweenX('wtfMain8', 3, 715, 2.3, 'expoOut')
 								
 								if downscroll then
-									noteTweenY('yoPeakDown1', 0, defaultPlayerStrumY0 - 220, 2.3, 'expoOut')
-									noteTweenY('yoPeakDown2', 1, defaultPlayerStrumY1 - 220, 2.3, 'expoOut')
-									noteTweenY('yoPeakDown3', 2, defaultPlayerStrumY2 - 220, 2.3, 'expoOut')
-									noteTweenY('yoPeakDown4', 3, defaultPlayerStrumY3 - 220, 2.3, 'expoOut')
-								end
-								if upscroll then
-									noteTweenY('yoPeakDown1', 0, defaultPlayerStrumY0 + 220, 2.3, 'expoOut')
-									noteTweenY('yoPeakDown2', 1, defaultPlayerStrumY1 + 220, 2.3, 'expoOut')
-									noteTweenY('yoPeakDown3', 2, defaultPlayerStrumY2 + 220, 2.3, 'expoOut')
-									noteTweenY('yoPeakDown4', 3, defaultPlayerStrumY3 + 220, 2.3, 'expoOut')
+									noteTweenY('yoPeakDown1', 0, defaultPlayerStrumY0 - 620, 2.3, 'expoOut')
+									noteTweenY('yoPeakDown2', 1, defaultPlayerStrumY1 - 620, 2.3, 'expoOut')
+									noteTweenY('yoPeakDown3', 2, defaultPlayerStrumY2 - 620, 2.3, 'expoOut')
+									noteTweenY('yoPeakDown4', 3, defaultPlayerStrumY3 - 620, 2.3, 'expoOut')
+								else
+									noteTweenY('yoPeakUp1', 0, defaultPlayerStrumY0 + 620, 2.3, 'expoOut')
+									noteTweenY('yoPeakUp2', 1, defaultPlayerStrumY1 + 620, 2.3, 'expoOut')
+									noteTweenY('yoPeakUp3', 2, defaultPlayerStrumY2 + 620, 2.3, 'expoOut')
+									noteTweenY('yoPeakUp4', 3, defaultPlayerStrumY3 + 620, 2.3, 'expoOut')
 								end
 								noteTweenAngle('hahaAngleGoBRRR1', 0, 360, 2.3, 'expoOut')
 								noteTweenAngle('hahaAngleGoBRRR2', 1, 360, 2.3, 'expoOut')
@@ -45,9 +52,9 @@ function onBeatHit()
 		
 								-- player strums go here (this forces middle scroll!@!!@!@!@)
 								noteTweenX('wtfMain9', 4, 415, 2.3, 'expoOut')
-								noteTweenX('wtfMain10', 5, 525, 2.3, 'expoOut')
-								noteTweenX('wtfMain11', 6, 625, 2.3, 'expoOut')
-								noteTweenX('wtfMain12', 7, 725, 2.3, 'expoOut')
+								noteTweenX('wtfMain10', 5, 515, 2.3, 'expoOut')
+								noteTweenX('wtfMain11', 6, 615, 2.3, 'expoOut')
+								noteTweenX('wtfMain12', 7, 715, 2.3, 'expoOut')
 								noteTweenAngle('hahaAngleGoBRRR5', 4, 360, 2.3, 'expoOut')
 								noteTweenAngle('hahaAngleGoBRRR6', 5, 360, 2.3, 'expoOut')
 								noteTweenAngle('hahaAngleGoBRRR7', 6, 360, 2.3, 'expoOut')
@@ -78,11 +85,12 @@ if ballSackReal == true then
         setProperty('camGame.zoom', 1.05)
         doTweenZoom('Beat2', 'camHUD', 1, 3, 'cubeOut')
         doTweenZoom('Beat1', 'camHUD', 1, 3, 'cubeIn')
-						if curBeat >= 1 == 0 then
+						if curBeat >= 4 then
 								noteTweenAngle('anger1', 4, 360, 0.2, 'linear')
 								noteTweenAngle('anger2', 5, 360, 0.2, 'linear')
 								noteTweenAngle('anger3', 6, 360, 0.2, 'linear')
 								noteTweenAngle('anger4', 7, 360, 0.2, 'linear')
+								
 						else
 								noteTweenAngle('anger5', 4, -360, 0.5, 'linear')
 								noteTweenAngle('anger6', 5, -360, 0.5, 'linear')
